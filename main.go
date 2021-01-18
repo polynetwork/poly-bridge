32812d8301d4cf11354ccc0ec2094bb3e403f140
gaoyuan tan <chain_dev@163.com>
2021-01-18 12:59:33 +0800

    cros

--- main.go
+++ main.go
@@ -44,9 +44,9 @@ func main() {
 	}
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 		AllowAllOrigins: true,
-		AllowMethods: []string{"*"},
-		AllowHeaders: []string{"*"},
-		ExposeHeaders: []string{"Content-Length"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders: []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
 		AllowCredentials: true,}))
 	beego.Run()
 }
