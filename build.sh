ec8e43944e59a68d42a41bd8e31bc2aac67a4727
gaoyuan tan <chain_dev@163.com>
2021-03-01 12:10:44 +0800

    update

--- build.sh
+++ build.sh
@@ -70,6 +70,7 @@ cp ./../conf/src_transactions.json ./../$base/bridge_tools
 cp ./../conf/poly_transactions.json ./../$base/bridge_tools
 cp ./../conf/dst_transactions.json ./../$base/bridge_tools
 cp ./../conf/remove_transactions.json ./../$base/bridge_tools
+cp ./../conf/tokens.json ./../$base/bridge_tools
 
 cd ./../cmd
 go build -tags $tag -o bridge_server main.go
