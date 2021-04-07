96186fcc47f62cd0d753e3dbeddb0eee63630daf
gaoyuan tan <chain_dev@163.com>
2021-04-07 19:35:42 +0800

    update

--- controllers/transaction_controller.go
+++ controllers/transaction_controller.go
@@ -106,7 +106,6 @@ func (c *TransactionController) getTransactionByHash(hash string) (*models.SrcPo
 	res := db.Table("src_transactions").
 		Select("src_transactions.hash as src_hash, poly_transactions.hash as poly_hash, dst_transactions.hash as dst_hash, src_transactions.chain_id as chain_id, src_transfers.asset as token_hash").
 		Where("src_transactions.hash = ?", hash).
-		Joins("inner join wrapper_transactions on src_transactions.hash = wrapper_transactions.hash").
 		Joins("left join src_transfers on src_transactions.hash = src_transfers.tx_hash").
 		Joins("left join poly_transactions on src_transactions.hash = poly_transactions.src_hash").
 		Joins("left join dst_transactions on poly_transactions.hash = dst_transactions.poly_hash").
