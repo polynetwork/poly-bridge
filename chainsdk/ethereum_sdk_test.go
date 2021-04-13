bea28f7f1b74c74bbef054264173bff7c8e3cb68
dylenfu <dylenfu@126.com>
2021-04-13 10:59:15 +0800

    add test

--- chainsdk/ethereum_sdk_test.go
+++ chainsdk/ethereum_sdk_test.go
@@ -7,36 +7,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewEthereumSdk_GetTokensByIndex(t *testing.T) {
+func TestNewEthereumSdk_GetTokens(t *testing.T) {
+	t.Logf("current context: %s", ctx.EthUrl)
+
 	start := 0
 	length := 10
-	data, err := ethSdk.GetTokensByIndex(wrapContract, asset, owner, start, length)
+	balance, err := ctx.SDK.GetNFTBalance(ctx.Asset, ctx.Owner)
 	assert.NoError(t, err)
-	for tokenId, url := range data {
-		t.Logf("token %d url %s", tokenId.Uint64(), url)
-	}
-}
+	t.Logf("user NFT balance %d", balance.Uint64())
 
-func TestNewEthereumSdk_GetTokensById(t *testing.T) {
-	tokenIds := []*big.Int{
-		big.NewInt(10),
-		big.NewInt(12),
-		big.NewInt(13),
-		big.NewInt(14),
-		big.NewInt(16),
-		big.NewInt(17),
+	if balance.Uint64() == 0 {
+		return
 	}
 
-	data, err := ethSdk.GetTokensById(wrapContract, asset, tokenIds)
+	tokensByIndex, err := ctx.SDK.GetTokensByIndex(ctx.WrapAddress, ctx.Asset, ctx.Owner, start, length)
 	assert.NoError(t, err)
-	for tokenId, url := range data {
-		t.Logf("token %d url %s", tokenId.Uint64(), url)
-	}
-}
 
-func TestNewEthereumSdk_GetAndCheckTokenUrl(t *testing.T) {
-	tokenId := big.NewInt(12)
-	url, err := ethSdk.GetAndCheckTokenUrl(wrapContract, asset, owner, tokenId)
+	tokenIds := make([]*big.Int, 0)
+	for tokenId, url := range tokensByIndex {
+		t.Logf("getTokensByIndex: token %d url %s", tokenId.Uint64(), url)
+		tokenIds = append(tokenIds, tokenId)
+	}
+
+	tokensWithId, err := ctx.SDK.GetTokensById(ctx.WrapAddress, ctx.Asset, tokenIds)
 	assert.NoError(t, err)
-	t.Logf("token %d url is %s", tokenId.Uint64(), url)
+	for tokenId, url := range tokensWithId {
+		t.Logf("getTokensById: token %d url %s", tokenId.Uint64(), url)
+	}
+
+	for _, tokenId := range tokenIds {
+		url, err := ctx.SDK.GetAndCheckTokenUrl(ctx.WrapAddress, ctx.Asset, ctx.Owner, tokenId)
+		assert.NoError(t, err)
+		t.Logf("getAndCheckTokenUrl: token %d url is %s", tokenId.Uint64(), url)
+	}
 }
