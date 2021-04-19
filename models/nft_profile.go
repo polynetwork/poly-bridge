b0b03444e9f68d9bbde15688d8c566537584d678
dylenfu <dylenfu@126.com>
2021-04-19 19:03:56 +0800

    nfthttp:modify tokenid type

--- models/nft_profile.go
+++ models/nft_profile.go
@@ -2,7 +2,7 @@ package models
 
 type NFTProfile struct {
 	TokenBasicName string  `gorm:"primaryKey;size:64;not null"`
-	NftTokenId     *BigInt `gorm:"primaryKey;type:varchar(64);not null"`
+	NftTokenId     string  `gorm:"primaryKey;type:varchar(64);not null"`
 	Name           string  `gorm:"size:64;not null"`
 	Url            string  `gorm:"size:64;not null"`
 	Image          string  `gorm:"size:64;not null"`
