e50dbe06c33a20b678141f65051223e4e66cd319
dylenfu <dylenfu@126.com>
2021-06-01 11:03:55 +0800

    go_abi: update query abi

--- go_abi/nft_lock_proxy_abi/nft_lock_proxy_abi.go
+++ go_abi/nft_lock_proxy_abi/nft_lock_proxy_abi.go
@@ -5174,3 +5174,4 @@ func (_ZeroCopySource *ZeroCopySourceTransactorRaw) Transfer(opts *bind.Transact
 func (_ZeroCopySource *ZeroCopySourceTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
 	return _ZeroCopySource.Contract.contract.Transact(opts, method, params...)
 }
+
--- go_abi/nft_wrap_abi/nft_wrap_abi.go
+++ go_abi/nft_wrap_abi/nft_wrap_abi.go
@@ -7073,3 +7073,4 @@ func (_ZeroCopySource *ZeroCopySourceTransactorRaw) Transfer(opts *bind.Transact
 func (_ZeroCopySource *ZeroCopySourceTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
 	return _ZeroCopySource.Contract.contract.Transact(opts, method, params...)
 }
+
