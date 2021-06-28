29d6ecd939c9ffdf54eec10368921c7c157608dc
devfans <stefanliu@outlook.com>
2021-06-28 18:17:33 +0800

    Fix build script

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ fi
 
 go build -tags $tag -o bridge_server ./cmd
 go build -tags $tag -o bridge_http ./
+go build -tags $tag -o tools ./bridge_tools
 
 
 if [ ! -d "./$base/bridge_server" ]; then
@@ -19,15 +20,21 @@ if [ ! -d "./$base/bridge_http" ]; then
   mkdir -p "./$base/bridge_http"
 fi
 
+if [ ! -d "./$base/bridge_tools" ]; then
+  mkdir -p "./$base/bridge_tools"
+fi
+
+
 mv bridge_server ./$base/bridge_server/bridge_server
 mv bridge_http ./$base/bridge_http/bridge_http
+mv tools ./$base/bridge_tools/bridge_tools
 
 if [ "$tag"x = "mainnet"x ]
 then
   cp ./conf/config_mainnet.json ./../$base/bridge_server/
   cp ./conf/config_mainnet.json ./../$base/bridge_http/
 else
-  cp ./conf/config_testnet.json ./../$base/bridge_server/
-  cp ./conf/config_testnet.json ./../$base/bridge_http/
+  cp ./conf/config_testnet.json ./$base/bridge_server/
+  cp ./conf/config_testnet.json ./$base/bridge_http/
 fi
 
