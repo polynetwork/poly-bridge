55ed4860556d2b5c6f77d96fd25a84a517837966
rain <206131521@qq.com>
2021-09-22 10:43:53 +0800

    open transfer

--- crosschainlisten/ethereumlisten/ethereum.go
+++ crosschainlisten/ethereumlisten/ethereum.go
@@ -237,9 +237,10 @@ func (this *EthereumChainListen) HandleNewBlock(height uint64) ([]*models.Wrappe
 					break
 				}
 			}
-			if srcTransaction.SrcTransfer != nil || srcTransaction.SrcSwap != nil {
-				srcTransactions = append(srcTransactions, srcTransaction)
-			}
+			//opensrcTransactions
+			//if srcTransaction.SrcTransfer != nil || srcTransaction.SrcSwap != nil {
+			srcTransactions = append(srcTransactions, srcTransaction)
+			//}
 		}
 	}
 	// save unLockEvent to db
@@ -282,9 +283,10 @@ func (this *EthereumChainListen) HandleNewBlock(height uint64) ([]*models.Wrappe
 					dstTransaction.DstTransfer.Standard = models.TokenTypeErc721
 				}
 			}
-			if dstTransaction.DstTransfer != nil {
-				dstTransactions = append(dstTransactions, dstTransaction)
-			}
+			//opendstTransactions
+			//if dstTransaction.DstTransfer != nil {
+			dstTransactions = append(dstTransactions, dstTransaction)
+			//}
 		}
 	}
 	return wrapperTransactions, srcTransactions, nil, dstTransactions, len(proxyLockEvents), len(proxyUnlockEvents), nil
