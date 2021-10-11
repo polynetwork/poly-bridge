dc9c220e96ff37f839a3258371f9bd955c07f15c
rain <206131521@qq.com>
2021-10-11 14:52:38 +0800

    censusAssetLines chain

--- crosschaindao/bridgedao/bridge_dao.go
+++ crosschaindao/bridgedao/bridge_dao.go
@@ -538,13 +538,14 @@ func (dao *BridgeDao) GetAvgTimePoly2Dst(timeLast, timeNow int64) ([]*ChainAvgTi
 
 type TokenStatisticWithName struct {
 	TokenBasicName string
+	ChainId        uint64
 	InAmount       *models.BigInt
 	InAmountUsd    *models.BigInt
 }
 
 func (dao *BridgeDao) GetSourceTokenStatistics() ([]*TokenStatisticWithName, error) {
 	sourceTokenStatistics := make([]*TokenStatisticWithName, 0)
-	err := dao.db.Raw("SELECT b.token_basic_name,a.in_amount,a.in_amount_usd from token_statistics a left join tokens b on a.chain_id=b.chain_id and a.`hash`=b.`hash` left join token_basics c on b.token_basic_name=c.`name` where c.chain_id=a.chain_id").
+	err := dao.db.Raw("SELECT b.token_basic_name,a.chain_id,a.in_amount,a.in_amount_usd from token_statistics a left join tokens b on a.chain_id=b.chain_id and a.`hash`=b.`hash` left join token_basics c on b.token_basic_name=c.`name` where c.chain_id=a.chain_id").
 		Find(&sourceTokenStatistics).Error
 	return sourceTokenStatistics, err
 }
