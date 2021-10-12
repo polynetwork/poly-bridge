ec7f5d0c24d93ade919ee0b3b67adb5045529e25
rain <206131521@qq.com>
2021-10-12 18:52:35 +0800

    coinpricelisten

--- cmd/main.go
+++ cmd/main.go
@@ -90,7 +90,7 @@ func startServer(ctx *cli.Context) {
 	if config.Backup {
 		return
 	}
-	//coinpricelisten.StartCoinPriceListen(config.Server, config.CoinPriceUpdateSlot, config.CoinPriceListenConfig, config.DBConfig)
+	coinpricelisten.StartCoinPriceListen(config.Server, config.CoinPriceUpdateSlot, config.CoinPriceListenConfig, config.DBConfig)
 	chainfeelisten.StartFeeListen(config.Server, config.FeeUpdateSlot, config.FeeListenConfig, config.DBConfig)
 	crosschaineffect.StartCrossChainEffect(config.Server, config.EventEffectConfig, config.DBConfig, config.RedisConfig)
 	crosschainstats.StartCrossChainStats(config.Server, config.StatsConfig, config.DBConfig, config.IPPortConfig)
