33a08f265a9f3b17442b515c32873ee3524a2908
wuyachi <wuyachi@163.com>
2021-10-26 15:26:32 +0800

    FormatFee

--- models/utils.go
+++ models/utils.go
@@ -174,6 +174,9 @@ func FormatFee(chain uint64, fee *BigInt) string {
 			return feeString + " POA"
 		}
 		return feeString + " XDai"
+	case basedef.OPTIMISTIC_CROSSCHAIN_ID:
+		precision_new := decimal.New(1, 18)
+		return fee_new.Div(precision_new).String() + " ETH"
 	default:
 		precision_new := decimal.New(int64(1), 0)
 		return fee_new.Div(precision_new).String()
