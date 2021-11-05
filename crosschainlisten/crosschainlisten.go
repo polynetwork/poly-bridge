dfeabd09b90cdb22763447ffc01e0a02e1848cb2
wuyachi <wuyachi@163.com>
2021-11-05 15:02:05 +0800

    Revert "fix duplicate large tx alarm"
    This reverts commit f531353467e5c4a328fa14b874b3ce0426ca71ac.

--- crosschainlisten/crosschainlisten.go
+++ crosschainlisten/crosschainlisten.go
@@ -21,11 +21,9 @@ import (
 	"fmt"
 	"github.com/shopspring/decimal"
 	"math"
-	"poly-bridge/cacheRedis"
 	"poly-bridge/common"
 	"runtime/debug"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/polynetwork/bridge-common/metrics"
@@ -284,11 +282,6 @@ func (ccl *CrossChainListen) checkLargeTransaction(srcTransactions []*models.Src
 	}
 	if srcTransactions != nil && len(srcTransactions) > 0 {
 		for _, v := range srcTransactions {
-			if existed, err := cacheRedis.Redis.Exists(cacheRedis.LargeTxAlarmPrefix + strings.ToLower(v.Hash)); err == nil && existed {
-				logs.Info("large TX hash: %s alarm has been sent.", v.Hash)
-				return
-			}
-
 			if v.SrcTransfer != nil {
 				token, err := ccl.db.GetTokenBasicByHash(v.SrcTransfer.ChainId, v.SrcTransfer.Asset)
 				if err == nil {
@@ -300,10 +293,6 @@ func (ccl *CrossChainListen) checkLargeTransaction(srcTransactions []*models.Src
 					if amount.Cmp(decimal.NewFromInt(ccl.config.LargeTxAmount)) >= 0 {
 						if err := ccl.sendLargeTransactionDingAlarm(v, token, ccl.config.IPPortConfig.LargeTxAmountAlarmDingIP, ccl.config.LargeTxAmount, amount); err != nil {
 							logs.Error("send BigTxAmount alert err.", err)
-						} else {
-							if _, err := cacheRedis.Redis.Set(cacheRedis.LargeTxAlarmPrefix+strings.ToLower(v.Hash), "done", time.Hour); err != nil {
-								logs.Error("mark large TX hash: %s alarm done err: %s", v.Hash, err)
-							}
 						}
 					}
 				}
