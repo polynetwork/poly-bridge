4792c368429f8115ac7b42287b528cd6b3372efe
rain <206131521@qq.com>
2021-11-30 15:10:47 +0800

    cross native token

---
+++ crossnativetoken/http/info_controller.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 The poly network Authors
+ * This file is part of The poly network library.
+ *
+ * The  poly network  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The  poly network  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The poly network .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package http
+
+import (
+	"poly-bridge/models"
+
+	"github.com/beego/beego/v2/server/web"
+)
+
+type InfoController struct {
+	web.Controller
+}
+
+func (c *InfoController) Get() {
+	explorer := &models.PolyBridgeResp{
+		Version: "v1",
+		URL:     "http://localhost:8080/v1",
+	}
+	c.Data["json"] = explorer
+	c.ServeJSON()
+}
