9a696164aa413121d96beec8037e2b739d181862
rain <206131521@qq.com>
2021-12-08 11:35:15 +0800

    delete nums logs

--- http/fee_controller.go
+++ http/fee_controller.go
@@ -96,10 +96,6 @@ func (c *FeeController) GetFee() {
 		}
 		tokenBalance, _ := new(big.Int).SetString("100000000000000000000000000000", 10)
 		if tokenMap.DstChainId != basedef.PLT_CROSSCHAIN_ID {
-			if tokenMap.DstTokenHash == "0000000000000000000000000000000000000000" {
-				getfeeethnum++
-				logs.Error("getfeeethnum is:", getfeeethnum)
-			}
 			tokenBalance, err = cacheRedis.Redis.GetTokenBalance(tokenMap.DstChainId, tokenMap.DstTokenHash)
 			if err != nil {
 				tokenBalance, err = common.GetBalance(tokenMap.DstChainId, tokenMap.DstTokenHash)
