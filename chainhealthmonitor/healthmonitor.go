1fb2581963bd3c17251629899676256a2b4da4fe
wuyachi <wuyachi@163.com>
2021-12-31 17:31:47 +0800

    update

--- chainhealthmonitor/healthmonitor.go
+++ chainhealthmonitor/healthmonitor.go
@@ -20,6 +20,7 @@ import (
 	"poly-bridge/conf"
 	"poly-bridge/go_abi/eccm_abi"
 	"runtime/debug"
+	"strconv"
 	"time"
 )
 
@@ -97,9 +98,17 @@ func sendNodeStatusDingAlarm(nodeStatus basedef.NodeStatus) error {
 		nodeStatus.Height,
 		nodeStatus.Status,
 	)
-	buttons := make([]map[string]string, 0)
+	//buttons := make([]map[string]string, 0)
 	logs.Info(body)
-	return polycommon.PostDingCard(title, body, buttons, conf.GlobalConfig.BotConfig.LargeTxDingUrl)
+
+	ss := title + "\n"
+	ss += "Node: " + nodeStatus.Url
+	ss += "Height: " + strconv.FormatUint(nodeStatus.Height, 10)
+	ss += "Status: " + nodeStatus.Status
+
+	return polycommon.PostDingtext(ss, conf.GlobalConfig.BotConfig.LargeTxDingUrl)
+
+	//return polycommon.PostDingCard(title, body, buttons, conf.GlobalConfig.BotConfig.LargeTxDingUrl)
 }
 
 func NewHealthMonitorHandle(monitorConfig *conf.HealthMonitorConfig) HealthMonitorHandle {
