1a15082fb16b6c62e0462f4729b48380b1ae90f1
wuyachi <wuyachi@163.com>
2022-01-12 19:38:33 +0800

    update

--- http/fee_controller.go
+++ http/fee_controller.go
@@ -130,7 +130,7 @@ func (c *FeeController) GetFee() {
 				case basedef.ETHEREUM_CROSSCHAIN_ID, basedef.O3_CROSSCHAIN_ID, basedef.BSC_CROSSCHAIN_ID, basedef.PLT_CROSSCHAIN_ID,
 					basedef.OK_CROSSCHAIN_ID, basedef.HECO_CROSSCHAIN_ID, basedef.MATIC_CROSSCHAIN_ID, basedef.ARBITRUM_CROSSCHAIN_ID,
 					basedef.XDAI_CROSSCHAIN_ID, basedef.FANTOM_CROSSCHAIN_ID, basedef.AVAX_CROSSCHAIN_ID, basedef.OPTIMISTIC_CROSSCHAIN_ID,
-					basedef.METIS_CROSSCHAIN_ID:
+					basedef.METIS_CROSSCHAIN_ID, basedef.BOBA_CROSSCHAIN_ID, basedef.RINKEBY_CROSSCHAIN_ID, basedef.PIXIE_CROSSCHAIN_ID:
 					var dstLockProsxies []string
 					for _, cfg := range conf.GlobalConfig.ChainListenConfig {
 						if cfg.ChainId == tokenMap.DstChainId {
