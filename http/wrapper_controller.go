48c32c14f47e8ba6a0af21358cdf4bc2abc6787d
rain-zxn <206131521@qq.com>
2022-05-07 23:04:59 +0800

    wrapper check

---
+++ http/wrapper_controller.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/beego/beego/v2/server/web"
+	"poly-bridge/models"
+)
+
+type WrapperController struct {
+	web.Controller
+}
+
+func (c *WrapperController) WrapperCheck() {
+	var wrapperCheckReq models.WrapperCheckReq
+	var err error
+	if err = json.Unmarshal(c.Ctx.Input.RequestBody, &wrapperCheckReq); err != nil {
+		c.Data["json"] = models.MakeErrorRsp(fmt.Sprintf("request parameter is invalid!"))
+		c.Ctx.ResponseWriter.WriteHeader(400)
+		c.ServeJSON()
+		return
+	}
+	wrappers, ok := wrapperContract[wrapperCheckReq.ChainId]
+	if !ok {
+		c.Data["json"] = models.MakeErrorRsp(fmt.Sprintf("no this chain!"))
+		c.Ctx.ResponseWriter.WriteHeader(400)
+		c.ServeJSON()
+		return
+	}
+	wrapperCheckRsp := &models.WrapperCheckRsp{
+		wrapperCheckReq.ChainId,
+		wrappers,
+	}
+	c.Data["json"] = wrapperCheckRsp
+	c.ServeJSON()
+}
