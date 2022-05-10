d21e72facffac959f729b61cda29800b1420a682
rain-zxn <206131521@qq.com>
2022-05-10 15:12:53 +0800

    ont evm listen

--- crosschainlisten/polylisten/poly.go
+++ crosschainlisten/polylisten/poly.go
@@ -103,7 +103,7 @@ func (this *PolyChainListen) HandleNewBlock(height uint64) ([]*models.WrapperTra
 				mctx.SrcChainId = uint64(fchainid)
 				mctx.DstChainId = uint64(tchainid)
 				switch uint64(fchainid) {
-				case basedef.NEO_CROSSCHAIN_ID, basedef.NEO3_CROSSCHAIN_ID, basedef.ONT_CROSSCHAIN_ID:
+				case basedef.NEO_CROSSCHAIN_ID, basedef.NEO3_CROSSCHAIN_ID, basedef.ONT_CROSSCHAIN_ID, basedef.ONTEVM_CROSSCHAIN_ID:
 					mctx.SrcHash = basedef.HexStringReverse(states[3].(string))
 				default:
 					mctx.SrcHash = states[3].(string)
