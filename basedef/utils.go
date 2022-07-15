7b3ae79b57d692cd530eaf95e4d89eaa84e786dc
rain-zxn <206131521@qq.com>
2022-07-15 10:58:07 +0800

    add ripple (update FormatAddr and FormatTxHash)

--- basedef/utils.go
+++ basedef/utils.go
@@ -323,7 +323,7 @@ func GetChainName(id uint64) string {
 
 func FormatAddr(chain uint64, addr string) string {
 	switch chain {
-	case ONT_CROSSCHAIN_ID, SWITCHEO_CROSSCHAIN_ID, ZILLIQA_CROSSCHAIN_ID, NEO_CROSSCHAIN_ID, NEO3_CROSSCHAIN_ID:
+	case ONT_CROSSCHAIN_ID, SWITCHEO_CROSSCHAIN_ID, ZILLIQA_CROSSCHAIN_ID, NEO_CROSSCHAIN_ID, NEO3_CROSSCHAIN_ID, RIPPLE_CROSSCHAIN_ID:
 		return addr
 	case STARCOIN_CROSSCHAIN_ID:
 		if Has0xPrefix(addr) {
@@ -337,7 +337,7 @@ func FormatAddr(chain uint64, addr string) string {
 
 func FormatTxHash(chain uint64, hash string) string {
 	switch chain {
-	case ONT_CROSSCHAIN_ID:
+	case ONT_CROSSCHAIN_ID, RIPPLE_CROSSCHAIN_ID:
 		return hash
 	case SWITCHEO_CROSSCHAIN_ID:
 		return strings.ToUpper(hash)
