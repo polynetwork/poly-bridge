7fb14389dea1210cdacf602ab544d204a979243e
rain-zxn <206131521@qq.com>
2022-07-19 18:04:17 +0800

    add Conflux eSpace chain

--- models/utils.go
+++ models/utils.go
@@ -267,6 +267,9 @@ func FormatFee(chain uint64, fee *BigInt) string {
 	case basedef.CLOVER_CROSSCHAIN_ID:
 		precision_new := decimal.New(1, 18)
 		return fee_new.Div(precision_new).String() + " CLV"
+	case basedef.CONFLUX_CROSSCHAIN_ID:
+		precision_new := decimal.New(1, 18)
+		return fee_new.Div(precision_new).String() + " CFX"
 	default:
 		precision_new := decimal.New(int64(1), 0)
 		return fee_new.Div(precision_new).String()
